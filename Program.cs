78465abe3b76fdde763c3e459536d79703832fd5
Matthew Carney <matthewcarney64@gmail.com>
2022-11-18 23:41:57 +0000

    Initial parser work Template finalised Schema finalised

--- Program.cs
+++ Program.cs
@@ -5,6 +5,75 @@
 using System.Drawing.Imaging;
 using System.Drawing;
 
+// Load the schema file
+string pathToSchemaFile = "SCHEMA";
+string[] schemaFileContents;
+try
+{
+    schemaFileContents = File.ReadAllLines(pathToSchemaFile);
+    DebugLog($"Schema file loaded ({pathToSchemaFile})");
+}
+catch (Exception e)
+{
+    DebugError($"[{e.GetType()}] Could not read schema file - {e.Message}");
+    return;
+}
+
+// Parse schema
+enum SchemaTokens
+{
+    BaseUrl,
+    PageUrl,
+    Location,
+    Title,
+    Month,
+    Year,
+    Author,
+    Camera,
+    ClassIdImageGrid,
+    ClassIdImageColumn,
+    ClassIdImageElement,
+    Image,
+    DetailedImage
+};
+
+Dictionary<SchemaTokens, string> tokenDictionary = new()
+{
+    { SchemaTokens.BaseUrl, "%BASE_URL" },
+};
+
+for (int i = 0; i < schemaFileContents.Length; i++)
+{
+    DebugLog(schemaFileContents[i]);
+}
+
+// Load the template file
+// TODO: We should do this first
+string pathToTemplateFile = "template.html";
+string[] templateFileContents;
+try
+{
+    templateFileContents = File.ReadAllLines(pathToTemplateFile);
+    DebugLog($"Template file loaded ({pathToTemplateFile})");
+}
+catch (Exception e)
+{
+    DebugError($"[{e.GetType()}] Could not read template file - {e.Message}");
+    return;
+}
+
+// Parse the template file
+for (int i = 0; i < templateFileContents.Length; i++)
+{
+    DebugLog(templateFileContents[i]);
+}
+for (int i = 0; i < schemaFileContents.Length; i++)
+{
+    DebugLog(schemaFileContents[i]);
+}
+return;
+
+
 string directory = Directory.GetCurrentDirectory();
 string[] images = Directory.GetFiles(directory, "*.jpg");
 
@@ -14,5 +83,18 @@ foreach (string jpeg in images)
     {
         
     }
-        Console.WriteLine(jpeg);
+}
+
+
+void DebugLog(string message)
+{
+    Console.WriteLine($"[{DateTime.Now.ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'ffff")}] {message}");
+    Console.ResetColor();
+}
+
+void DebugError(string message)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"[{DateTime.Now.ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'ffff")}] {message}");
+    Console.ResetColor();
 }
